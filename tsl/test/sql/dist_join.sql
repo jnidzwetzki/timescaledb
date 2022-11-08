1edbc854bcbd54b7266abc0a42ee9b8a553617a3
Jan Nidzwetzki <jan@timescale.com>
2022-11-08 13:20:49 +0100

    Replaced tree walker with stats

--- tsl/test/sql/dist_join.sql
+++ tsl/test/sql/dist_join.sql
@@ -23,18 +23,28 @@ GRANT USAGE ON FOREIGN SERVER :DATA_NODE_1, :DATA_NODE_2, :DATA_NODE_3 TO PUBLIC
 
 drop table if exists metric;
 create table metric(ts timestamptz, id int, value float);
-
 select create_distributed_hypertable('metric', 'ts', 'id');
 insert into metric values ('2022-02-02 02:02:02+03', 1, '50');
 
+-- The reference table
 create table metric_name(id int primary key, name text);
 insert into metric_name values (1, 'cpu1');
 
-CALL distributed_exec($$ create table metric_name(id int primary key, name text); $$);
+CALL distributed_exec($$create table metric_name(id int primary key, name text);$$);
 CALL distributed_exec($$insert into metric_name values (1, 'cpu1');$$);
 
 create table metric_name_local(id int primary key, name text);
 insert into metric_name_local values (1, 'cpu1');
+
+create table dummy_table(id int primary key, name text);
+CALL distributed_exec($$create table dummy_table(id int primary key, name text);$$);
+
+create table dummy_table_local(id int primary key, name text);
+
+
+-- ALTER FOREIGN DATA WRAPPER timescaledb_fdw OPTIONS (SET reference_tables 'metric_name, dummy_table');
+
+SET client_min_messages TO DEBUG1;
 
 explain (verbose, analyze)
 select name, value from metric 
@@ -42,6 +52,7 @@ left join metric_name using (id)
 where name like 'cpu%'
 and ts between '2022-02-02 02:02:02+03' and '2022-02-02 02:12:02+03';
 
+-- Join with reference table
 explain (verbose, analyze)
 select name, max(value) from metric 
 left join metric_name using (id)
@@ -49,10 +60,26 @@ where name like 'cpu%'
 and ts between '2022-02-02 02:02:02+03' and '2022-02-02 02:12:02+03'
 group by name;
 
-
+-- Join with reference table
 explain (verbose, analyze)
 select name, max(value) from metric, metric_name
 where metric.id = metric_name.id
+and name like 'cpu%'
+and ts between '2022-02-02 02:02:02+03' and '2022-02-02 02:12:02+03'
+group by name;
+
+-- Join with local table
+explain (verbose, analyze)
+select name, max(value) from metric 
+left join metric_name_local using (id)
+where name like 'cpu%'
+and ts between '2022-02-02 02:02:02+03' and '2022-02-02 02:12:02+03'
+group by name;
+
+-- Join with local table
+explain (verbose, analyze)
+select name, max(value) from metric, metric_name_local
+where metric.id = metric_name_local.id
 and name like 'cpu%'
 and ts between '2022-02-02 02:02:02+03' and '2022-02-02 02:12:02+03'
 group by name;
